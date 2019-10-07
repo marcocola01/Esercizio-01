7676c92a5db98a406e91674a2b8095448de1ed6e
marcocola01 <colangelo.marco@iisgalvanimi.edu.it>
2019-10-07 09:01:09 +0000

    esercizio 01.1 completo

---
+++ ESEMPIO_EXPRESS/EXPRESS_STATIC_FILES/app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+const path = require('path');
+
+app.get('/about',function(req,res){
+ res.sendFile(path.join(__dirname+'/about.html'));
+});
+
+app.get('/index',function(req,res){
+ res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.get('/sitemap',function(req,res){
+ res.sendFile(path.join(__dirname+'/sitemap.html'));
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
